44038c3244a5d95aac279d336048c224aa615655
Jesus Redondo <github@jesusredondo.com>
2024-10-08 16:57:39 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillMember() {
+    var member = {
+        name: "John",
+        age: 30,
+        skills: ["js", "html", "css"],
+        details: {
+            address: "123 Main St",
+            city: "New York"
+        }
+    };
+    return member;
+}
